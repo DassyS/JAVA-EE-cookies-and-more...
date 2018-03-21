4510995a5155d5fa2ce7f78bab09ba976ffd5cb3
DassyS <37508795+DassyS@users.noreply.github.com>
2018-03-21 15:42:02 +0200

    Update FirstPage.java

--- FirstPage.java
+++ FirstPage.java
@@ -23,6 +23,12 @@ public class FirstPage extends HttpServlet {
 			HttpServletResponse response) throws ServletException, IOException {
 		 String backgroundColor =  CookieUtilities.getCookieValue(request, "BackgroundColor","White");
 		 String foregroundColor = CookieUtilities.getCookieValue(request, "ForegroundColor","Black");
+		 Cookie cookieID = new LongLivedCookie("UserID", userID);
+		 response.addCookie(cookieID);
+		 Cookie cookieBack = new LongLivedCookie("BackgroundColor", backgroundColor);
+		 response.addCookie(cookieBack);
+		 Cookie cookieFore = new LongLivedCookie("ForegroundColor",foregroundColor);
+		 response.addCookie(cookieFore);
 		 PrintWriter out = response.getWriter();
 
 		response.setContentType("text/html");
